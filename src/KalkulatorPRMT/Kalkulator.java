c83d272bbcc04ae1bd3caaa6e044365b7cc1414c
@mpodgajn <michal.podgajny.stud@pw.edu.pl>
2021-05-21 18:12:36 +0200

    Próba dodania nawiasów 3.0

--- src/KalkulatorPRMT/Kalkulator.java
+++ src/KalkulatorPRMT/Kalkulator.java
@@ -16,12 +16,19 @@ public class Kalkulator {
 
         ZbiorWyrazen zbior = new ZbiorWyrazen();
 
+        System.out.println("Aby dodać wyrażenia lub przypisania zmiennych po prostu je napisz i zatwierdź enterem");
+        System.out.println("Jeżeli chcesz wykonać wyrażenie to wpisz komende 'do' i zatwierdź enterem");
+        System.out.println("");
         while (!wyrazenie.equals("do")) {
             Scanner scan = new Scanner(System.in);
             wyrazenie = scan.nextLine();
 
             if(!wyrazenie.equals("do")) zbior.add(wyrazenie);
         }
+
+        System.out.println("");
+        System.out.println("Wykonuje działania. Raport poniżej");
+        System.out.println("");
 
         zbior.rozwiaz();
 
