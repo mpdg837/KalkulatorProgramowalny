c4b480f2622c84b406cf708245238c0fd4732938
@mpodgajn <michal.podgajny.stud@pw.edu.pl>
2021-05-31 13:53:15 +0200

    Dodanie poleceń i działań 2.0

--- src/KalkulatorPRMT/Obliczanie/Przetwarzanie/Grupowanie.java
+++ src/KalkulatorPRMT/Obliczanie/Przetwarzanie/Grupowanie.java
@@ -31,6 +31,110 @@ public class Grupowanie {
         this.ciagDzilania = ciag;
     }
 
+
+    // Metoda służy do wykonywania działań oraz funkcji
+
+    public StringBuilder dodajWynik(StringBuilder nowazaw,String zawartosc) throws MyError{
+
+        // Definiowanie miejsc na komendy
+        char[] znaki2 = nowazaw.toString().toCharArray();
+
+        StringBuilder komenda2 = new StringBuilder();
+        StringBuilder komenda3 = new StringBuilder();
+        StringBuilder komenda4 = new StringBuilder();
+
+        // Wydobywanie komend dwuznakowych
+        for(int n=znaki2.length-2;n<znaki2.length;n++)
+            if (n >= 0) {
+                komenda2.append(znaki2[n]);
+            }
+
+        // Trzyznakowych
+        for(int n=znaki2.length-3;n<znaki2.length;n++)
+            if (n >= 0) {
+                komenda3.append(znaki2[n]);
+            }
+
+
+        // Czteroznakowych
+        for(int n=znaki2.length-4;n<znaki2.length;n++)
+            if (n >= 0) {
+                komenda4.append(znaki2[n]);
+            }
+
+        boolean czyWykrytoKomende = false;
+
+        if(komenda2.toString().toLowerCase().equals("ln")){
+
+            nowazaw = new StringBuilder();
+
+            // Czyszczę StringBuildera z zbędnych znaków
+            for(int n=0;n<znaki2.length-2;n++){
+                nowazaw.append(znaki2[n]);
+            }
+
+            // Wykonuję logarytm liniowy
+            nowazaw.append(DodatekMatematyczny.ln(rozwiazSkladowa(zawartosc)));
+
+            czyWykrytoKomende = true;
+        }
+        // Analiza komend 3 znakowych
+        switch (komenda3.toString()) {
+            case "sin", "cos", "tan", "abs", "exp", "log","pow" -> {
+                nowazaw = new StringBuilder();
+
+                // Czyszczę stringbuildera z niepotrzebnych znaków
+                for (int n = 0; n < znaki2.length - 3; n++) {
+                    nowazaw.append(znaki2[n]);
+                }
+
+                // Wykonuję działanie
+                switch (komenda3.toString().toLowerCase()) {
+                    case "sin" -> nowazaw.append(Math.sin(rozwiazSkladowa(zawartosc)));
+                    case "cos" -> nowazaw.append(Math.cos(rozwiazSkladowa(zawartosc)));
+                    case "tan" -> nowazaw.append(Math.tan(rozwiazSkladowa(zawartosc)));
+                    case "abs" -> nowazaw.append(Math.abs(rozwiazSkladowa(zawartosc)));
+                    case "exp" -> nowazaw.append(Math.exp(rozwiazSkladowa(zawartosc)));
+                    case "log" -> nowazaw.append(DodatekMatematyczny.log(rozwiazSkladowa(zawartosc)));
+                    case "pow" -> nowazaw.append(Math.pow(rozwiazSkladowa(zawartosc), 2));
+                }
+                czyWykrytoKomende = true;
+            }
+        }
+
+        // Analiza komend 4 znakowych
+        switch (komenda4.toString()) {
+            case "sqrt", "pow2", "pow3", "fact" -> {
+
+                // Czyszczę StringBuildera z zbędnych znaków
+                nowazaw = new StringBuilder();
+                for (int n = 0; n < znaki2.length - 4; n++) {
+                    nowazaw.append(znaki2[n]);
+                }
+
+                // Wykonuję działanie
+                switch (komenda4.toString().toLowerCase()) {
+                    case "sqrt" -> nowazaw.append(DodatekMatematyczny.sqrt(rozwiazSkladowa(zawartosc)));
+                    case "pow2" -> nowazaw.append(Math.pow(rozwiazSkladowa(zawartosc), 2));
+                    case "pow3" -> nowazaw.append(Math.pow(rozwiazSkladowa(zawartosc), 3));
+                    case "fact" -> nowazaw.append(DodatekMatematyczny.fact(rozwiazSkladowa(zawartosc)));
+
+
+                }
+
+                czyWykrytoKomende = true;
+            }
+        }
+
+        if(!czyWykrytoKomende) {
+
+            // Gdy nie wykryto komendy
+            nowazaw.append(rozwiazSkladowa(zawartosc));
+        }
+
+        return nowazaw;
+
+    }
     public Double wynik(){
         try {
             // Zmienna infromująca o poziomie zagnieżgdżenia wyrażeń rozwiązywanych przez klasy nieobsługujące nawiasów
@@ -64,6 +168,7 @@ public class Grupowanie {
                     case ")" -> {
                         String zawartosc = poziomy.get(stopienzagniezdzenia).toString();
                         poziomy.set(stopienzagniezdzenia, new StringBuilder());
+
                         if (stopienzagniezdzenia > 0) {
                             // Obniżam poziom zagnieżdżenia
                             stopienzagniezdzenia--;
@@ -76,101 +181,7 @@ public class Grupowanie {
                         // Dodanie wyniku obliczania wnętrza nawiasu
                         StringBuilder nowazaw = poziomy.get(stopienzagniezdzenia);
 
-                        // Definiowanie miejsc na komendy
-                        char[] znaki2 = nowazaw.toString().toCharArray();
-
-                        StringBuilder komenda2 = new StringBuilder();
-                        StringBuilder komenda3 = new StringBuilder();
-                        StringBuilder komenda4 = new StringBuilder();
-
-                        // Wydobywanie komend dwuznakowych
-                        for(int n=znaki2.length-2;n<znaki2.length;n++)
-                            if (n >= 0) {
-                                komenda2.append(znaki2[n]);
-                            }
-
-                        // Trzyznakowych
-                        for(int n=znaki2.length-3;n<znaki2.length;n++)
-                            if (n >= 0) {
-                                komenda3.append(znaki2[n]);
-                            }
-
-
-                        // Czteroznakowych
-                        for(int n=znaki2.length-4;n<znaki2.length;n++)
-                            if (n >= 0) {
-                                komenda4.append(znaki2[n]);
-                            }
-
-                        boolean czyWykrytoKomende = false;
-
-                        if(komenda2.toString().toLowerCase().equals("ln")){
-
-                            nowazaw = new StringBuilder();
-
-                            // Czyszczę StringBuildera z zbędnych znaków
-                            for(int n=0;n<znaki2.length-2;n++){
-                                nowazaw.append(znaki2[n]);
-                            }
-
-                            // Wykonuję logarytm liniowy
-                            nowazaw.append(DodatekMatematyczny.ln(rozwiazSkladowa(zawartosc)));
-
-                            czyWykrytoKomende = true;
-                        }
-                        // Analiza komend 3 znakowych
-                        switch (komenda3.toString()) {
-                            case "sin", "cos", "tan", "abs", "exp", "log" -> {
-                                nowazaw = new StringBuilder();
-
-                                // Czyszczę stringbuildera z niepotrzebnych znaków
-                                for (int n = 0; n < znaki2.length - 3; n++) {
-                                    nowazaw.append(znaki2[n]);
-                                }
-
-                                // Wykonuję działanie
-                                switch (komenda3.toString().toLowerCase()) {
-                                    case "sin" -> nowazaw.append(Math.sin(rozwiazSkladowa(zawartosc)));
-                                    case "cos" -> nowazaw.append(Math.cos(rozwiazSkladowa(zawartosc)));
-                                    case "tan" -> nowazaw.append(Math.tan(rozwiazSkladowa(zawartosc)));
-                                    case "abs" -> nowazaw.append(Math.abs(rozwiazSkladowa(zawartosc)));
-                                    case "exp" -> nowazaw.append(Math.exp(rozwiazSkladowa(zawartosc)));
-                                    case "log" -> nowazaw.append(DodatekMatematyczny.log(rozwiazSkladowa(zawartosc)));
-                                    case "pow" -> nowazaw.append(Math.pow(rozwiazSkladowa(zawartosc), 2));
-                                }
-                                czyWykrytoKomende = true;
-                            }
-                        }
-
-                        // Analiza komend 4 znakowych
-                        switch (komenda4.toString()) {
-                            case "sqrt", "pow2", "pow3", "fact" -> {
-
-                                // Czyszczę StringBuildera z zbędnych znaków
-                                nowazaw = new StringBuilder();
-                                for (int n = 0; n < znaki2.length - 4; n++) {
-                                    nowazaw.append(znaki2[n]);
-                                }
-
-                                // Wykonuję działanie
-                                switch (komenda4.toString().toLowerCase()) {
-                                    case "sqrt" -> nowazaw.append(DodatekMatematyczny.sqrt(rozwiazSkladowa(zawartosc)));
-                                    case "pow2" -> nowazaw.append(Math.pow(rozwiazSkladowa(zawartosc), 2));
-                                    case "pow3" -> nowazaw.append(Math.pow(rozwiazSkladowa(zawartosc), 3));
-                                    case "fact" -> nowazaw.append(DodatekMatematyczny.fact(rozwiazSkladowa(zawartosc)));
-
-
-                                }
-
-                                czyWykrytoKomende = true;
-                            }
-                        }
-
-                        if(!czyWykrytoKomende) {
-
-                            // Gdy nie wykryto komendy
-                            nowazaw.append(rozwiazSkladowa(zawartosc));
-                        }
+                        nowazaw = dodajWynik(nowazaw,zawartosc);
 
                         poziomy.set(stopienzagniezdzenia, nowazaw);
                     }
