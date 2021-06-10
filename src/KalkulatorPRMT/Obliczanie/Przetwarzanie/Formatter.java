93ab47c43dcecb8e6e354b7d9cdbc60afa91c60f
@mpodgajn <michal.podgajny.stud@pw.edu.pl>
2021-06-10 10:43:47 +0200

    Usprawnienie formatu liczb

--- src/KalkulatorPRMT/Obliczanie/Przetwarzanie/Formatter.java
+++ src/KalkulatorPRMT/Obliczanie/Przetwarzanie/Formatter.java
@@ -3,6 +3,9 @@ package KalkulatorPRMT.Obliczanie.Przetwarzanie;
 import KalkulatorPRMT.Obliczanie.MyError;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Formatter {
 
@@ -10,11 +13,15 @@ public class Formatter {
         if(Math.abs(num)<0.0000001){
             return "0";
         }else{
+            DecimalFormatSymbols symbole = new DecimalFormatSymbols(Locale.GERMAN);
+            symbole.setDecimalSeparator('.');
 
-            DecimalFormat dec = new DecimalFormat("##################################.#############################################");
+            DecimalFormat dec = new DecimalFormat("##################################.#############################################",symbole);
             if(Math.abs(num)>2147483647){
                 throw new MyError("Za duży wynik");
             }
+
+            System.out.println(dec.format(num));
             return dec.format(num);
         }
     }
