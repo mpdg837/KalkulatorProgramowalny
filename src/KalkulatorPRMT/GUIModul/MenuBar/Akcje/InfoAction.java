cdf72ca3241eab8f583ccf14c202d03f4f939dfa
@mpodgajn <michal.podgajny.stud@pw.edu.pl>
2021-06-15 10:04:37 +0200

    Informacje o zakończeniu projektu

--- src/KalkulatorPRMT/GUIModul/MenuBar/Akcje/InfoAction.java
+++ src/KalkulatorPRMT/GUIModul/MenuBar/Akcje/InfoAction.java
@@ -8,6 +8,6 @@ public class InfoAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(new JLabel(),"Kalkulator 0.9. Wersja beta, testowa, niepełna. \n Projekt zaliczeniowy w ramach przedmiotu PRM2T, w semestrze 21L (rok akademicki 2020/2021) \n na kierunku Telekomunikacja na Wydziale Elektorniki i Technik Informacyjnych Politechniki Warszawskiej. \n \n Autorzy \n \n Michał Podgajny \n Jędrzej Joniec \n Michał Kamiński \n \n Czerwiec 2021","Informacje",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JLabel(),"Kalkulator 1.0. Wersja prezentacyjna. \n Projekt zaliczeniowy w ramach przedmiotu PRM2T, w semestrze 21L (rok akademicki 2020/2021) \n na kierunku Telekomunikacja na Wydziale Elektorniki i Technik Informacyjnych Politechniki Warszawskiej. \n \n Autorzy \n \n Michał Podgajny \n Jędrzej Joniec \n Michał Kamiński \n \n Czerwiec 2021","Informacje",JOptionPane.PLAIN_MESSAGE);
     }
 }
